7ba51521e19503bd980b8299194572ae044698bb
lewis nyoike <nyoikelewis@gmail.com>
2017-05-30 12:54:19 +0300

    needs some work

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -1,12 +1,12 @@
 var apiKey = require('./../.env').apiKey;
-var Lookup = require('./../js/lookup.js').lookupModule;
+var Lookup = require('./../js/lookup.js').lookupModule;.reposModule;
 
 $(document).ready(function() {
   var lookupObject = new Lookup();
+  var reposObject = new repos();
   $("#search").click(function() {
-
     var name = $('#lookup').val();
-    $('#lookup').val("");
     lookupObject.getLookup(name);
+    reposObject.getRepos(repos);
   });
 });
--- js/lookup.js
+++ js/lookup.js
@@ -2,14 +2,31 @@ var apiKey = require('./../.env').apiKey;
 
 
 Lookup = function() {};
+Repos = function() {};
 
 Lookup.prototype.getLookup = function(name) {
-  var name = $("#lookup").val("");
-  $.get('https://api.github.com/users/Luey-Ny0ike?access_token=' + apiKey).then(function(response) {
-    console.log(response);
+  $.get('https://api.github.com/users/' + name + '?access_token=' + apiKey).then(function(response) {
+    $(".place").text(response.name);
+
   }).fail(function(error) {
     console.log(error.responseJSON.message);
   });
 };
 
+Lookup.prototype.getLookup = function(name) {
+  $.get('https://api.github.com/users/' + name + '/repos?access_token=' + apiKey).then(function(response) {
+    {
+      for (var i = 0; i < response.length + 1; i++) {
+        if (response[i].description === null) {
+          response[i].description = 'No description in repo';
+        }
+        displayRepos(response[i].name, response[i].description, response[i].created_at);
+      }
+    }).fail(function(error) {
+      //console.log(error.response.message);
+      console.log("errorname");
+    });
+  })
+};
 exports.lookupModule = Lookup;
+exports.reposModule = repos;
